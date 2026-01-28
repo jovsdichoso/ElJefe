c5fa1ac5c3dcc600730a0e14f81856f24af33474
Jovs <papidichoso.jd@gmail.com>
2026-01-28 15:51:03 +0800

    polish

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // src/App.jsx
-import { HashRouter, Routes, Route } from 'react-router-dom'; // <--- CHANGE THIS IMPORT
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
-import About from './pages/About'; // Don't forget to import About
+import About from './pages/About';
 import Header from './components/Header';
 
 const Admin = () => <h1 className="text-3xl text-white text-center mt-20">Admin Login</h1>;
